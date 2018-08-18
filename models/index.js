9a42522b01b2d727363ba7984ef6b8310ce7f8e6
JShafer92 <ptwraith1@gmail.com>
2018-08-18 11:35:06 -0700

    added scss files to repo

--- models/index.js
+++ models/index.js
@@ -1,46 +1,21 @@
 "use strict";
 
-<<<<<<< HEAD
-var fs = require("fs");
-var path = require("path");
-var io = require("socket.io")(http);
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
-=======
 const fs = require("fs");
 const path = require("path");
+const io = require("socket.io")(http);
 const Sequelize = require("sequelize");
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
->>>>>>> b8d720c5298f4a28daf4690fb15442d081eac260
 
-if (config.use_env_variable) {
-	var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (config.use_env_constiable) {
+	let sequelize = new Sequelize(process.env[config.use_env_constiable]);
 } else {
-	var sequelize = new Sequelize(config.database, config.username, config.password, config);
+	let sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
-<<<<<<< HEAD
-  .readdirSync(__dirname)
-  .filter(function (file) {
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-  })
-  .forEach(function (file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(function (modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-=======
 	.readdirSync(__dirname)
 	.filter(function(file) {
 		return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
@@ -54,7 +29,6 @@ Object.keys(db).forEach(function(modelName) {
 	if (db[modelName].associate) {
 		db[modelName].associate(db);
 	}
->>>>>>> b8d720c5298f4a28daf4690fb15442d081eac260
 });
 
 db.sequelize = sequelize;
