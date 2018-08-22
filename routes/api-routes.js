77c55452fe103dcda32082e74e78ccda10c26be7
Jacob <jrnels10@gmail.com>
2018-08-21 19:47:01 -0700

    updating port of master

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,16 +13,16 @@ const db = require("../models");
 module.exports = function (app) {
 	// GET route for getting all of the posts
 	app.get("/api/future_locations/", function (req, res) {
-		db.TravelTable.findAll({})
-			.then(function (TravelTable) {
-				res.json(TravelTable);
-				console.log(TravelTable);
+		db.traveltables.findAll({})
+			.then(function (traveltables) {
+				res.json(traveltables);
+				console.log(traveltables);
 			});
 	});
 
 	app.post("/api/future_locations", function (req, res) {
 		console.log(req.body);
-		db.TravelTable.create({
+		db.traveltables.create({
 			username: req.body.username,
 			email: req.body.email,
 			image: req.body.image,
@@ -32,32 +32,7 @@ module.exports = function (app) {
 			// res.redirect("/user_map?" + "username=" + dbPost.username);
 		});
 	});
-	// app.put("/api/future_locations", function (req, res) {
-	// 	console.log(req.body);
-	// 	db.TravelTable.update({
-	// 		image: req.body.image,
-	// 		future_location: req.body.future_location
-	// 	}, {
-	// 			where: {
-	// 				id: req.body.username
-	// 			}
-	// 		}).then(function (dbTodo) {
-	// 			res.json(dbTodo);
-	// 		});
-	// });
-
-
-	// db.TravelTable.findOne({ username: req.body.username }).then(function(){
-	// 	db.TravelTable.update({
-
-	// 	})
-	// })({
-	// 	username: req.body.username,
-	// 	email: req.body.email,
-	// 	}).then(function (dbPost) {
-	// 		res.json(dbPost);
-	// 	});
-	// });
+	
 
 
 };
