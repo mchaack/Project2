4f6c599e4efcfdabcc58e7b4d4997b268a99e268
Angel Lugo <this@angellugo.com>
2018-08-25 09:53:23 -0700

    updated the db dump

--- db/angel.sql
+++ db/angel.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `project_two` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `project_two`;
 -- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: project_two
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-25  9:49:20
+-- Dump completed on 2018-08-25  9:52:54
