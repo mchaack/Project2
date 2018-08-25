88b8ba9ee663b8ed3466f8380196ad74014f6000
Jacob <jrnels10@gmail.com>
2018-08-24 21:28:55 -0700

    map looks good

--- public/js/get.js
+++ public/js/get.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 			attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, " +
 				"<a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, " +
 				"Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-			id: "mapbox.light"
+			id: "mapbox.dark"
 		}).addTo(map);
 
 
